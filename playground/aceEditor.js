a62b54c5209e94ddab6089c0bcfd208b4991197a
ido <idok@wix.com>
2014-11-27 18:53:37 +0200

    add tabs to playground

--- playground/aceEditor.js
+++ playground/aceEditor.js
@@ -1,7 +1,7 @@
 /**
  * Created by avim on 11/25/2014.
  */
-
+'use strict';
 var React = require('react/addons');
 var _ = require('lodash');
 var brace = require('brace');
@@ -11,29 +11,29 @@ require('brace/mode/html');
 require('brace/theme/monokai');
 
 var editor = React.createClass({
-    diplayName:"BraceEditor",
+    displayName: 'BraceEditor',
     getInitialState: function () {
         return {
             editorId: _.uniqueId()
-        }
+        };
     },
     componentWillMount: function () {
 
     },
     render: function () {
-        var props = _.omit(this.props,['id','ref','key','value','valueLink','onChange']);
+        var props = _.omit(this.props, ['id', 'ref', 'key', 'value', 'valueLink', 'onChange']);
         props.id = this.state.editorId;
-        return React.DOM.div(props)
+        return React.DOM.div(props);
     },
-    componentWillUpdate: function (nextProps, nextState) {
-        var value = nextProps.valueLink?nextProps.valueLink():nextProps.value;
-        if (this.editor && this.editor.getValue() != value ) {
-            this.editor.setValue(value,0);
+    componentWillUpdate: function (nextProps/*, nextState*/) {
+        var value = nextProps.valueLink ? nextProps.valueLink() : nextProps.value;
+        if (this.editor && this.editor.getValue() != value) {
+            this.editor.setValue(value, 0);
         }
     },
     componentDidMount: function () {
         this.editor = brace.edit(this.state.editorId);
-        if (this.props.mode !== "html") {
+        if (this.props.mode !== 'html') {
             this.editor.getSession().setMode('ace/mode/javascript');
         } else {
             this.editor.getSession().setMode('ace/mode/html');
@@ -41,17 +41,17 @@ var editor = React.createClass({
         this.editor.getSession().setUseWorker(false);
         this.editor.setTheme('ace/theme/monokai');
 
-        var value = this.props.valueLink?this.props.valueLink():this.props.value;
-        this.editor.setValue(value,0);
+        var value = this.props.valueLink ? this.props.valueLink() : this.props.value;
+        this.editor.setValue(value, 0);
         if (this.props.readOnly) {
             this.editor.setReadOnly(true);
         } else {
             this.editor.setReadOnly(false);
-            this.editor.on("change", function(e) {
+            this.editor.on('change', function (/*e*/) {
                 if (this.props.valueLink) {
                     this.props.valueLink(this.editor.getValue());
                 } else if (this.props.onChange) {
-                    this.props.onChange({target:{value:this.editor.getValue()}});
+                    this.props.onChange({target: {value: this.editor.getValue()}});
                 }
             }.bind(this));
         }
--- playground/playground.rt.js
+++ playground/playground.rt.js
@@ -8,27 +8,70 @@ function onChange1(evt) {
 function onChange2(evt) {
     this.setState({ 'templateProps': evt.target.value });
 }
+function onSubmit3(e) {
+    e.preventDefault();
+}
 module.exports = function () {
-    return React.DOM.div({}, React.DOM.div({ 'className': 'code-area' }, React.DOM.h2({}, 'Template:'), CodeEditor({
+    return React.DOM.div({}, React.DOM.div({
+        'id': 'myTab',
+        'role': 'tabpanel',
+        'className': 'code-area'
+    }    /*  Nav tabs  */, React.DOM.ul({
+        'className': 'nav nav-pills',
+        'role': 'tablist'
+    }, React.DOM.li({
+        'role': 'presentation',
+        'className': 'active'
+    }, React.DOM.a({
+        'href': '#template',
+        'aria-controls': 'template',
+        'role': 'tab',
+        'data-toggle': 'tab'
+    }, 'Template')), React.DOM.li({ 'role': 'presentation' }, React.DOM.a({
+        'href': '#classCode',
+        'aria-controls': 'classCode',
+        'role': 'tab',
+        'data-toggle': 'tab'
+    }, 'Class')), React.DOM.li({ 'role': 'presentation' }, React.DOM.a({
+        'href': '#generatedCode',
+        'aria-controls': 'generatedCode',
+        'role': 'tab',
+        'data-toggle': 'tab'
+    }, 'Generated code')))    /*  Tab panes  */, React.DOM.div({ 'className': 'tab-content' }, React.DOM.div({
+        'role': 'tabpanel',
+        'className': 'tab-pane active',
+        'id': 'template'
+    }, CodeEditor({
         'className': 'large-text-area',
         'style': { border: this.validHTML ? '1px solid black' : '2px solid red' },
         'value': this.state.templateHTML,
         'mode': 'html',
         'onChange': onChange1.bind(this)
-    }), React.DOM.br({}), React.DOM.h2({}, 'Class:'), CodeEditor({
+    })), React.DOM.div({
+        'role': 'tabpanel',
+        'className': 'tab-pane',
+        'id': 'classCode'
+    }, CodeEditor({
         'className': 'large-text-area',
         'style': { border: this.validProps ? '1px solid black' : '2px solid red' },
         'value': this.state.templateProps,
         'mode': 'javascript',
         'onChange': onChange2.bind(this)
     })), React.DOM.div({
-        'key': 'result-area',
-        'className': 'result-area'
-    }, React.DOM.h2({}, 'Generated code:'), CodeEditor({
+        'role': 'tabpanel',
+        'className': 'tab-pane',
+        'id': 'generatedCode'
+    }, CodeEditor({
         'className': 'large-text-area',
         'style': { border: '1px solid black' },
         'value': this.templateSource,
         'mode': 'javascript',
         'readOnly': true
-    }), React.DOM.br({}), React.DOM.h2({}, 'Preview:'), React.DOM.div({ 'className': 'sample-view' }, this.sample({ 'key': 'sample' }))));
+    })))), React.DOM.div({
+        'key': 'result-area',
+        'className': 'result-area well'
+    }, React.DOM.br({}), React.DOM.h2({}, 'Preview:'), React.DOM.form({
+        'className': 'sample-view',
+        'onSubmit': onSubmit3.bind(this)
+    }, this.sample({ 'key': 'sample' }))));
 };
