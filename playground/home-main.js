adda4e38779e18b944578ebda137517fff0d8fd9
ido <idok@wix.com>
2014-12-24 12:33:54 +0200

    lodash minimized

--- playground/home-main.js
+++ playground/home-main.js
@@ -1,7 +1,7 @@
 requirejs.config({
 //    baseUrl: '/',
     paths: {
-        lodash: 'http://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash',
+        lodash: 'http://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min',
         jquery: 'http://code.jquery.com/jquery-1.11.0.min',
         firebase: 'https://cdn.firebase.com/js/client/2.0.5/firebase',
         react: 'http://fb.me/react-with-addons-0.12.1',
