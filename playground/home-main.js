07c3952a2f0904f3bb80111a6cd7055037c1e842
ido <idok@wix.com>
2014-12-24 12:36:32 +0200

    react minimized

--- playground/home-main.js
+++ playground/home-main.js
@@ -4,7 +4,7 @@ requirejs.config({
         lodash: 'http://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min',
         jquery: 'http://code.jquery.com/jquery-1.11.0.min',
         firebase: 'https://cdn.firebase.com/js/client/2.0.5/firebase',
-        react: 'http://fb.me/react-with-addons-0.12.1',
+        react: 'http://fb.me/react-with-addons-0.12.1.min',
         text: 'libs/requirejs-plugins/text',
         json: 'libs/requirejs-plugins/json'
         //ace: '../ace-builds-1.1.8/src-min/ace',
