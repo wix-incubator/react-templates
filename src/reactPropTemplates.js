bde2f67e01d07e08064d6cee0b0039eca41f30cf
Antonino Porcino <nino.porcino@gmail.com>
2016-07-07 13:52:06 +0200

    Support for react native 0.29.0

--- src/reactPropTemplates.js
+++ src/reactPropTemplates.js
@@ -10,6 +10,16 @@ const native = {
             SectionHeader: {prop: 'renderSectionHeader', arguments: ['sectionData', 'sectionID']},
             Separator: {prop: 'renderSeparator', arguments: ['sectionID', 'rowID', 'adjacentRowHighlighted']}
         }
+    },
+    '0.29.0': {
+        ListView: {
+            Row: {prop: 'renderRow', arguments: ['rowData', 'sectionID', 'rowID', 'highlightRow']},
+            Footer: {prop: 'renderFooter', arguments: []},
+            Header: {prop: 'renderHeader', arguments: []},
+            ScrollComponent: {prop: 'renderScrollComponent', arguments: ['props']},
+            SectionHeader: {prop: 'renderSectionHeader', arguments: ['sectionData', 'sectionID']},
+            Separator: {prop: 'renderSeparator', arguments: ['sectionID', 'rowID', 'adjacentRowHighlighted']}
+        }
     }
 };
 
