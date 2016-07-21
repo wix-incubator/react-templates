f086cd63ead67b30510970fc3fdeda337ef1a0e5
ido <idok@wix.com>
2016-07-21 10:55:39 +0300

    fix test

--- test/data/propTemplates/siblingTemplates.rt.js
+++ test/data/propTemplates/siblingTemplates.rt.js
@@ -3,16 +3,16 @@ define([
     'lodash'
 ], function (React, _) {
     'use strict';
-    function templateProp11(brother) {
-        return React.createElement('div', {}, 'Brother: ', brother);
-    }
-    function templateProp22(sister) {
-        return React.createElement('div', {}, 'Sister: ', sister);
-    }
-    function templateProp33(other) {
-        return React.createElement('div', {}, 'Other: ', other);
-    }
     return function () {
+        function templateProp11(brother) {
+            return React.createElement('div', {}, 'Brother: ', brother);
+        }
+        function templateProp22(sister) {
+            return React.createElement('div', {}, 'Sister: ', sister);
+        }
+        function templateProp33(other) {
+            return React.createElement('div', {}, 'Other: ', other);
+        }
         return React.createElement('div', {
             'templateProp1': templateProp11.bind(this),
             'templateProp2': templateProp22.bind(this),
